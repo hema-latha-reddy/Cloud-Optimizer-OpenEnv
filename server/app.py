a0a1c29a5f1b73fe174aa82fca45034af72e40dd
Hemalatha <150412205+hema-latha-reddy@users.noreply.github.com>
2026-04-07 20:23:19 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from fastapi import FastAPI, Request
 import random
